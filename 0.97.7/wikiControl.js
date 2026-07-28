82768f8921f8b13dee9e704007a8a42e9a109b2b
MIOBOMB <miobomb.real@gmail.com>
2026-07-29 00:18:51 +0700

    guide patch 2

--- 0.97.7/wikiControl.js
+++ 0.97.7/wikiControl.js
@@ -559,7 +559,7 @@ editGuide = (guideId, wikiId, backpage = 0)=>{
 		let html = 
 		`<h1${getTrans('guides01')}/h1>`+
 		//(guidWin == 0 ? `<button type=button class=loginbtn onclick="${backpage === 1 ? `profilePage('');getGuidesAdminControl(${wikiId})` : `pageGuides(${wikiId})`}"${getTrans('otmena')}/button><br>` : '')+
-		`<form id=GDPSesPlace${guideId} style=padding:8px method=post onsubmit="return enterFormData(this,'${sData[1]}guideEdit${php}?id=${guideId}')">`+
+		`<form id=GDPSesPlace${guideId} style=padding:8px method=post onsubmit="return enterFormData(this,'${sData[1]}editGuide${php}?id=${guideId}')">`+
 			`<input name=title class=guidInp id=title${guideId} value="${guideinfo[1]}" style="width:calc(100% - 4px);font-size:calc(var(--def-font)*2)"${getTrans('guides02', 'input')}<br>`+
 			`<label${getTrans('gdpsLang00')}/label> `+
 			`<select id="langs${guideId}" class="framelabel" name="language" required>`+
